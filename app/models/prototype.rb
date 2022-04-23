61f86554de4430b67355b57e5eeed2cb8b07cf28
yuri050 <cheb.owo1182@gmail.com>
2022-04-23 17:35:59 +0900

    first commit

---
+++ app/models/prototype.rb
@@ -0,0 +1,10 @@
+class Prototype < ApplicationRecord
+  has_many :comments
+  has_one_attached :image
+  belongs_to :user
+
+  validates :title, presence: true
+  validates :catch_copy, presence: true
+  validates :concept, presence: true  
+  validates :image, presence: true
+end
